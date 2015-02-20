f17853e28c2271122fe2023cfda41252aae5fe6e
jdehlin <jdehlin@gmail.com>
2015-02-20 09:09:58 -0600

    Starting on separating async functionality

--- CacheSleeve.NET40/Properties/AssemblyInfo.cs
+++ CacheSleeve.NET40/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7f7d156d-6c24-4d2c-9a2c-fe3fe0c8b7db")]
+[assembly: Guid("f856c9bc-4492-46ff-bc03-179c8ea088ea")]
 
 // Version information for an assembly consists of the following four values:
 //
